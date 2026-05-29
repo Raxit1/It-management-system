a313e55ff18db8d55328319b6c317cf0d2fce428
Raxit Chandegra <raxitchandegra58@gmail.com>
2026-05-29 11:45:35 +0530

    fix database url for render deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,24 +1,17 @@
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from sqlalchemy.ext.declarative import (
-    declarative_base
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-from sqlalchemy.orm import sessionmaker
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is missing in Render environment variables")
 
-from app.config import DATABASE_URL
-
-
-engine = create_engine(
-    DATABASE_URL
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
-
     autocommit=False,
-
     autoflush=False,
-
     bind=engine
 )
 
