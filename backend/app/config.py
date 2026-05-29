60dc1c9e0001c9d4eb4db70962a2e75a3a4bfe2e
Raxit Chandegra <raxitchandegra58@gmail.com>
2026-05-29 10:57:45 +0530

    fix: proper gitignore for full stack project

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,39 +1,15 @@
 from dotenv import load_dotenv
-
 import os
-
 
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL"
-)
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
 
-SECRET_KEY = os.getenv(
-    "SECRET_KEY"
-)
-
-ALGORITHM = os.getenv(
-    "ALGORITHM"
-)
-
-MAIL_USERNAME = os.getenv(
-    "MAIL_USERNAME"
-)
-
-MAIL_PASSWORD = os.getenv(
-    "MAIL_PASSWORD"
-)
-
-MAIL_FROM = os.getenv(
-    "MAIL_FROM"
-)
-
-MAIL_PORT = int(
-    os.getenv("MAIL_PORT")
-)
-
-MAIL_SERVER = os.getenv(
-    "MAIL_SERVER"
-)
+MAIL_PORT = int(os.getenv("MAIL_PORT", "587"))  # SAFE DEFAULT
+MAIL_SERVER = os.getenv("MAIL_SERVER")
